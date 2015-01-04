281e0070d7dad281acb0eefe13573fc7b547abae
Ian Miell <ian.miell@gmail.com>
2015-01-04 16:44:28 +0000

    run script and libmng added

---
+++ libmng/libmng.py
@@ -0,0 +1,47 @@
+"""ShutIt module. See http://shutit.tk
+"""
+
+from shutit_module import ShutItModule
+
+
+class libmng(ShutItModule):
+
+
+	def is_installed(self, shutit):
+		return shutit.file_exists('/root/shutit_build/module_record/' + self.module_id + '/built')
+
+	def build(self, shutit):
+		shutit.send('mkdir -p /tmp/build/libmng')
+		shutit.send('cd /tmp/build/libmng')
+		shutit.send('wget -qO- http://downloads.sourceforge.net/libmng/libmng-2.0.2.tar.xz | xz -d | tar -xf -')
+		shutit.send('cd libmng*')
+		shutit.send(r'''sed -i "s:#include <jpeg:#include <stdio.h>\n&:" libmng_types.h''')
+		shutit.send('./configure --prefix=/usr --disable-static')
+		shutit.send('make')
+		shutit.send('make install')
+		shutit.send('install -v -m755 -d /usr/share/doc/libmng-2.0.2')
+		shutit.send('install -v -m644 doc/*.txt /usr/share/doc/libmng-2.0.2')
+		return True
+
+	#def get_config(self, shutit):
+	#	shutit.get_config(self.module_id,'item','default')
+	#	return True
+
+	def finalize(self, shutit):
+		shutit.send('rm -rf /tmp/build/libmng')
+		return True
+
+	#def remove(self, shutit):
+	#	return True
+
+	#def test(self, shutit):
+	#	return True
+
+def module():
+	return libmng(
+		'shutit.tk.sd.libmng.libmng', 158844782.012101,
+		description='',
+		maintainer='',
+		depends=['shutit.tk.sd.lcms.lcms']
+	)
+
--- llvm/llvm.py
+++ llvm/llvm.py
@@ -13,12 +13,28 @@ class llvm(ShutItModule):
 	def build(self, shutit):
 		shutit.send('mkdir -p /tmp/build/llvm')
 		shutit.send('cd /tmp/build/llvm')
+		shutit.send('wget http://llvm.org/releases/3.5.0/cfe-3.5.0.src.tar.xz')
+		shutit.send('wget http://llvm.org/releases/3.5.0/compiler-rt-3.5.0.src.tar.xz')
 		shutit.send('wget -qO- http://llvm.org/releases/' + shutit.cfg[self.module_id]['version'] + '/llvm-' + shutit.cfg[self.module_id]['version'] + '.src.tar.xz | xz -d | tar -xf -')
 		shutit.send('cd llvm-*')
+		shutit.send('tar -xf ../cfe-3.5.0.src.tar.xz -C tools')
+		shutit.send('tar -xf ../compiler-rt-3.5.0.src.tar.xz -C projects')
+		shutit.send('mv tools/cfe-3.5.0.src tools/clang')
+		shutit.send('mv projects/compiler-rt-3.5.0.src projects/compiler-rt')
 		shutit.send('sed -e "s:/docs/llvm:/share/doc/llvm-' + shutit.cfg[self.module_id]['version'] + ':" -i Makefile.config.in')
 		shutit.send('CC=gcc CXX=g++ ./configure --prefix=/usr --sysconfdir=/etc --enable-libffi --enable-optimized --enable-shared --disable-assertions')
 		shutit.send('make')
 		shutit.send('make install')
+		shutit.send('chmod -v 644 /usr/lib/libclang*.a')
+		shutit.send('chmod -v 644 /usr/lib/libLLVM*.a')
+		shutit.send('chmod -v 644 /usr/lib/libLTO*.a')
+		shutit.send('install -v -dm755 /usr/lib/clang-analyzer')
+		shutit.send('cp -rfv tools/clang/tools/scan-build /usr/lib/clang-analyzer/')
+		shutit.send('ln -sfv ../lib/clang-analyzer/scan-build/scan-build /usr/bin/')
+		shutit.send('cp -rfv tools/clang/tools/scan-view /usr/lib/clang-analyzer/')
+		shutit.send('ln -sfv ../lib/clang-analyzer/scan-view/scan-view /usr/bin/')
+		shutit.send('ln -sfv /usr/bin/clang /usr/lib/clang-analyzer/scan-build/')
+		shutit.send('mv -v /usr/lib/clang-analyzer/scan-build/scan-build.1 /usr/share/man/man1/')
 		return True
 
 	def get_config(self, shutit):
