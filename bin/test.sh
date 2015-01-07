17afbb346068fd74385fa36eb1ec229e228170aa
Ian Miell <ian.miell@gmail.com>
2015-01-07 08:13:25 +0000

    always build from base when testing

--- bin/test.sh
+++ bin/test.sh
@@ -11,6 +11,7 @@ pushd ..
 find . | grep .cnf$ | xargs chmod 0600
 popd
 
+export SHUTIT_OPTIONS="$SHUTIT_OPTIONS --image_tag imiell/sd_base"
 for d in make_certs lzo curl sharutils libgpg_error libgcrypt cpio which libffi libidn util_macros tcl check ruby procps_ng sqlite pcre python2 libgoogle_glog onigurama rsync icu sgml_common llvm apache_portable_runtime apache_portable_runtime_util libxslt xmlto less giflib asciidoc libarchive cmake alsa_lib python_pip scons serf apache go subversion nettle libtasn1 tls zip x7proto xcb_proto libxau libpng libxcb glib gobject freetype2_pre_harfbuzz harfbuzz freetype2 desktop_file_utils fontconfig opensp docbook_dsssl docbook_sgml_dtd openjade docbook_utils xorg_libs git atk linuxbrew jq tk cups nasm libjpeg libtiff pixman cairo pango gdk_pixbuf gtk2 lcms libmng libdbus java_binary ant junit nspr nss attr kona haproxy node boost libtirpc lsof sthttpd screen time tree openjdk expect berkeleydb linux_pam cyrus_sasl xinetd open_ldap mit_kerberos_v5 postgresql libdrm elfutils mesalib xbitmaps xcb_util xorg_apps xterm spector qt4 gflags rocksdb libevent libsnappy libpopt rpm fastcomp emscripten doxygen dpkg thrift mariadb emacs osquery erlang
 do
 	if [[ $started = "0" ]]
