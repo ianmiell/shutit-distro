1c1177083b58cd0b436e936682c6d4ef1c393abb
Ian Miell <ian.miell@gmail.com>
2015-01-08 08:14:34 +0000

    dpkg inexplicably not included

--- apt/apt.py
+++ apt/apt.py
@@ -17,19 +17,52 @@ class apt(ShutItModule):
 		shutit.send('cd /tmp/build/apt')
 		shutit.send('git clone git://anonscm.debian.org/apt/apt.git')
 		shutit.send('cd apt')
-		shutit.pause_point('')
 		shutit.send('cp /usr/share/automake-1.14/config.guess buildlib/')
 		shutit.send('cp /usr/share/automake-1.14/config.sub buildlib/')
-		#shutit.send('autoreconf -f -i') # don't think this is needed
-#checking for lzma.h... yes
+		shutit.send('autoreconf -f -i') # don't think this is needed
 #checking debian architecture... ./configure: line 4975: dpkg-architecture: command not found
 #configure: error: failed: use --host= or output from dpkg-architecture
-
 # hard-code line 4975 to amd64(?)
 #archset="x86_64-linux-gnu"
+		shutit.send('''sed -i 's/^archset=.*/archset="x86_64-linux-gnu"/' configure''')
 		shutit.send('./configure --prefix=/usr')
 		shutit.send('make')
+		shutit.pause_point('')
 # copy the bin objects to lib
+#imiell@lp01728:/space/git/dockerbook$ apt-file show apt | grep ^apt:
+#apt: /etc/apt/apt.conf.d/01autoremove
+#apt: /etc/apt/apt.conf.d/20changelog
+#apt: /etc/cron.daily/apt
+#apt: /etc/kernel/postinst.d/apt-auto-removal
+#apt: /etc/logrotate.d/apt
+#apt: /usr/bin/apt-cache
+#apt: /usr/bin/apt-cdrom
+#apt: /usr/bin/apt-config
+#apt: /usr/bin/apt-get
+#apt: /usr/bin/apt-key
+#apt: /usr/bin/apt-mark
+#apt: /usr/lib/apt/methods/bzip2
+#apt: /usr/lib/apt/methods/cdrom
+#apt: /usr/lib/apt/methods/copy
+#apt: /usr/lib/apt/methods/file
+#apt: /usr/lib/apt/methods/ftp
+#apt: /usr/lib/apt/methods/gpgv
+#apt: /usr/lib/apt/methods/gzip
+#apt: /usr/lib/apt/methods/http
+#apt: /usr/lib/apt/methods/lzma
+#apt: /usr/lib/apt/methods/mirror
+#apt: /usr/lib/apt/methods/rred
+#apt: /usr/lib/apt/methods/rsh
+#apt: /usr/lib/apt/methods/ssh
+#apt: /usr/lib/apt/methods/xz
+#apt: /usr/lib/dpkg/methods/apt/desc.apt
+#apt: /usr/lib/dpkg/methods/apt/install
+#apt: /usr/lib/dpkg/methods/apt/names
+#apt: /usr/lib/dpkg/methods/apt/setup
+#apt: /usr/lib/dpkg/methods/apt/update
+#apt: /usr/share/apt/ubuntu-archive.gpg
+#apt: /usr/share/bug/apt/script
+
 		#shutit.send('make install')
 		return True
 
@@ -59,6 +92,6 @@ def module():
 		'shutit.tk.sd.apt.apt', 158844782.02825,
 		description='',
 		maintainer='ian.miell@gmail.com',
-		depends=['shutit.tk.sd.curl.curl','shutit.tk.sd.xmlto.xmlto','shutit.tk.sd.berkeleydb.berkeleydb','shutit.tk.sd.git.git','shutit.tk.sd.gtest.gtest']
+		depends=['shutit.tk.sd.curl.curl','shutit.tk.sd.xmlto.xmlto','shutit.tk.sd.berkeleydb.berkeleydb','shutit.tk.sd.git.git','shutit.tk.sd.gtest.gtest','shutit.tk.sd.dpkg.dpkg']
 	)
 
