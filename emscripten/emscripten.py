b6bfb08c50b2cd570fc990c5ef9088ebd5c37558
Ian Miell <ian.miell@gmail.com>
2015-01-10 22:55:17 +0000

    emscripten

--- emscripten/emscripten.py
+++ emscripten/emscripten.py
@@ -11,22 +11,22 @@ class emscripten(ShutItModule):
 		return shutit.file_exists('/root/shutit_build/module_record/' + self.module_id + '/built')
 
 	def build(self, shutit):
-		shutit.send('mkdir -p /tmp/build/emscripten')
-		shutit.send('cd /tmp/build/emscripten')
-		shutit.send('wget -qO- https://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-portable.tar.gz | tar -zxf -')
-		shutit.send('cd emsdk*')
-		shutit.pause_point('emscripten build')
-		shutit.send('git clone https://github.com/kripken/emscripten.git')
-		shutit.send('cd emscripten')
 		# http://kripken.github.io/emscripten-site/docs/getting_started/downloads.html#sdk-download-and-install
 		# then: http://kripken.github.io/emscripten-site/docs/building_from_source/building_emscripten_from_source_on_linux.html
+		shutit.send('mkdir -p /opt')
+		shutit.send('cd /opt')
+		shutit.send('wget -qO- https://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-portable.tar.gz | tar -zxf -')
+		shutit.send('cd emsdk*')
+		shutit.send('./emsdk update')
+		shutit.send('./emsdk install latest')
+		shutit.send('./emsdk activate latest')
+		shutit.send('source ./emsdk_env.sh')
 		return True
 
 	def get_config(self, shutit):
 		return True
 
 	def finalize(self, shutit):
-		shutit.send('rm -rf /tmp/build/emscripten')
 		return True
 
 	#def remove(self, shutit):
