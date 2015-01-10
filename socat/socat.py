7abd69be3ab716f80b80f95f51c3c5b6604a1d19
Ian Miell <ian.miell@gmail.com>
2015-01-10 17:14:06 +0000

    socat

---
+++ socat/socat.py
@@ -0,0 +1,45 @@
+"""ShutIt module. See http://shutit.tk
+"""
+
+from shutit_module import ShutItModule
+
+
+class socat(ShutItModule):
+
+
+	def is_installed(self, shutit):
+		return shutit.file_exists('/root/shutit_build/module_record/' + self.module_id + '/built')
+
+	def build(self, shutit):
+		shutit.send('mkdir -p /tmp/build/socat')
+		shutit.send('cd /tmp/build/socat')
+		shutit.get_url('socat-1.4.0.3.tar.gz',['http://www.dest-unreach.org/socat/download'])
+		shutit.send('tar -zxf socat-1.4.0.3.tar.gz')
+		shutit.send('rm -f socat-1.4.0.3.tar.gz')
+		shutit.send('cd socat-*')
+		shutit.send('./configure --prefix=/usr')
+		shutit.send('make')
+		shutit.send('make MANDEST=/usr/share/man install')
+		return True
+
+	def get_config(self, shutit):
+		return True
+
+	def finalize(self, shutit):
+		shutit.send('rm -rf /tmp/build/socat')
+		return True
+
+	#def remove(self, shutit):
+	#	return True
+
+	#def test(self, shutit):
+	#	return True
+
+def module():
+	return socat(
+		'shutit.tk.sd.socat.socat', 158844782.0294,
+		description='',
+		maintainer='',
+		depends=['shutit.tk.setup']
+	)
+
