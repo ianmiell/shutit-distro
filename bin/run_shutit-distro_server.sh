ed71dd4e6b9d270d6293795de2888da1bc940ab9
Ian Miell <ian.miell@gmail.com>
2015-01-13 18:21:05 +0000

    shutit module

--- bin/run_shutit-distro_server.sh
+++ bin/run_shutit-distro_server.sh
@@ -1,7 +1,9 @@
 ARG=$1
 export SHUTIT_PORT=${ARG:-8080}
-while [ 1 ]; do shutit serve -m .. 2>&1 > /tmp/shutit-distro_server.log; sleep 2; done
+#while [ 1 ]; do shutit serve -m .. 2>&1 > /tmp/shutit-distro_server.log; sleep 2; done
 
+export N_SERVERS=100
 #Handy
-#for p in $(jot 10 8081); do echo SHUTIT_PORT=$p shutit serve -m .. \| tee /tmp/shutit-distro_out_$p \&;  done | sh
+CURRDIR=$(pwd)
+for p in $(jot $N_SERVERS 8100); do sleep 10 && cd $(dirname $(which shutit))/.. && echo SHUTIT_PORT=$p shutit serve -m $CURRDIR/.. --image_tag imiell/sd_base 2>&1 \| tee /tmp/shutit-distro_out_$p \&;  done | sh
 
---
+++ shutit/shutit.py
@@ -0,0 +1,28 @@
+"""ShutIt module. See http://shutit.tk
+"""
+
+from shutit_module import ShutItModule
+
+
+class shutit(ShutItModule):
+
+
+	def is_installed(self, shutit):
+		return shutit.file_exists('/root/shutit_build/module_record/' + self.module_id + '/built')
+
+	def build(self, shutit):
+		shutit.send('mkdir -p /opt')
+		shutit.send('cd /opt')
+		shutit.send('git clone https://github.com/ianmiell/shutit.git')
+		shutit.send('cd shutit')
+		shutit.send('pip install -r requirements.txt')
+		return True
+
+def module():
+	return shutit(
+		'shutit.tk.sd.shutit.shutit', 158844782.0299,
+		description='',
+		maintainer='',
+		depends=['shutit.tk.sd.git.git','shutit.tk.sd.curl.curl','shutit.tk.sd.python_pip.python_pip']
+	)
+
