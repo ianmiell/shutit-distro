a89c84802cd274d25b79b0d66d0852cb9061e920
Ian Miell <ian.miell@gmail.com>
2015-01-19 12:47:49 +0000

    react

---
+++ react/react.py
@@ -0,0 +1,27 @@
+"""ShutIt module. See http://shutit.tk
+"""
+
+from shutit_module import ShutItModule
+
+
+class react(ShutItModule):
+
+
+	def is_installed(self, shutit):
+		return shutit.file_exists('/root/shutit_build/module_record/' + self.module_id + '/built')
+
+	def build(self, shutit):
+		shutit.send('wget http://facebook.github.io/react/downloads/react-0.12.2.zip')
+		shutit.send('unzip react-0.12.2.zip')
+		shutit.send('npm install -g react-tools')
+		shutit.send('npm install -g react')
+		return True
+
+def module():
+	return react(
+		'shutit.tk.react.react.react.react', 158844782.0302,
+		description='',
+		maintainer='',
+		depends=['shutit.tk.sd.node.node']
+	)
+
