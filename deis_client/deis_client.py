152c02d77d9c0adbf28512474faacb33c58e22b1
Ian Miell <ian.miell@gmail.com>
2015-01-26 10:27:38 +0000

    deis_client/

---
+++ deis_client/deis_client.py
@@ -0,0 +1,47 @@
+"""ShutIt module. See http://shutit.tk
+"""
+
+from shutit_module import ShutItModule
+
+
+class deis_client(ShutItModule):
+
+
+	def is_installed(self, shutit):
+		return shutit.file_exists('/root/shutit_build/module_record/' + self.module_id + '/built')
+
+
+	def build(self, shutit):
+		shutit.send('mkdir -p /tmp/build/deis_client')
+		shutit.send('cd /tmp/build/deis_client')
+		shutit.send('curl -sSL http://deis.io/deis-cli/install.sh | sh')
+		shutit.send('ln -fs $PWD/deis /usr/bin/deis')
+		return True
+
+	def get_config(self, shutit):
+		return True
+
+	#def start(self, shutit):
+	#	return True
+
+	#def stop(self, shutit):
+	#    return True
+
+	def finalize(self, shutit):
+		shutit.send('rm -rf /tmp/build/deis_client')
+		return True
+
+	#def remove(self, shutit):
+	#	return True
+
+	#def test(self, shutit):
+	#	return True
+
+def module():
+	return deis_client(
+		'shutit.tk.sd.deis_client.deis_client', 158844782.0307,
+		description='deis_client',
+		maintainer='ian.miell@gmail.com',
+		depends=['shutit.tk.sd.curl.curl']
+	)
+
