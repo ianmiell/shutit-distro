032fef6c441e96770bd41ba6cb94002b25532422
Ian Miell <ian.miell@gmail.com>
2015-02-03 17:32:08 +0000

    rhc

---
+++ rhc/rhc.py
@@ -0,0 +1,41 @@
+"""ShutIt module. See http://shutit.tk
+"""
+
+from shutit_module import ShutItModule
+
+
+class rhc(ShutItModule):
+
+	def is_installed(self, shutit):
+		return shutit.file_exists('/root/shutit_build/module_record/' + self.module_id + '/built')
+
+	def build(self, shutit):
+		shutit.send('gem install rhc')
+		return True
+
+	def get_config(self, shutit):
+		return True
+
+	#def start(self, shutit):
+	#	return True
+
+	#def stop(self, shutit):
+	#    return True
+
+	def finalize(self, shutit):
+		return True
+
+	#def remove(self, shutit):
+	#	return True
+
+	#def test(self, shutit):
+	#	return True
+
+def module():
+	return rhc(
+		'shutit.tk.sd.rhc.rhc', 158844782.0318,
+		description='RedHat OpenShift Client tool',
+		maintainer='ian.miell@gmail.com',
+		depends=['shutit.tk.sd.ruby.ruby']
+	)
+
