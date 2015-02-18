c97d94d3f1373f76f5997e95725a871359501904
Ian Miell <ian.miell@gmail.com>
2015-02-18 12:17:20 +0000

    zip

--- zip/zip.py
+++ zip/zip.py
@@ -9,24 +9,17 @@ class zip(ShutItModule):
 	def build(self, shutit):
 		shutit.send('mkdir -p /tmp/build/zip')
 		shutit.send('cd /tmp/build/zip')
-		shutit.send('wget -qO- http://downloads.sourceforge.net/infozip/zip30.tar.gz | tar -zxf -')
+		f = 'zip30.tar.gz'
+		shutit.get_url(f,['http://pkgs.fedoraproject.org/repo/pkgs/zip/zip30.tar.gz/7b74551e63f8ee6aab6fbc86676c0d37/',''])
+		shutit.send('tar -zxf ' + f)
 		shutit.send('cd zip*')
 		shutit.send('make -f unix/Makefile generic_gcc')
 		shutit.send('make prefix=/usr MANDIR=/usr/share/man/man1 -f unix/Makefile install')
 		return True
 
-	def get_config(self, shutit):
-		return True
-
 	def finalize(self, shutit):
 		shutit.send('rm -rf /tmp/build/zip')
 		return True
-
-	#def remove(self, shutit):
-	#	return True
-
-	#def test(self, shutit):
-	#	return True
 
 def module():
 	return zip(
