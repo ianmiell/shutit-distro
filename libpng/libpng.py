09cb7c2103b3079058ab0c1c18487415853cefed
Ian Miell <ian.miell@gmail.com>
2015-02-18 12:05:14 +0000

    libpng

--- libpng/libpng.py
+++ libpng/libpng.py
@@ -9,7 +9,9 @@ class libpng(ShutItModule):
 	def build(self, shutit):
 		shutit.send('mkdir -p /tmp/build/libpng')
 		shutit.send('cd /tmp/build/libpng')
-		shutit.send('wget -qO- http://downloads.sourceforge.net/libpng/libpng-1.6.13.tar.xz | xz -d | tar -xf -')
+		f = 'libpng-1.6.13.tar.xz'
+		shutit.get_url(f,['http://ftp.osuosl.org/pub/blfs/conglomeration/libpng','http://downloads.sourceforge.net/libpng'])
+		shutit.send('xz -d ' + f + '| tar -xf -')
 		shutit.send('cd libpng*')
 		shutit.send('wget -QO- http://downloads.sourceforge.net/libpng-apng/libpng-1.6.13-apng.patch.gz | patch -p1 -')
 		shutit.send('./configure --prefix=/usr --disable-static')
@@ -17,19 +19,9 @@ class libpng(ShutItModule):
 		shutit.send('make install')
 		return True
 
-	#def get_config(self, shutit):
-	#	shutit.get_config(self.module_id,'item','default')
-	#	return True
-
 	def finalize(self, shutit):
 		shutit.send('rm -rf /tmp/build/libpng')
 		return True
-
-	#def remove(self, shutit):
-	#	return True
-
-	#def test(self, shutit):
-	#	return True
 
 def module():
 	return libpng(
